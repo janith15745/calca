c10e3c57cf8a67cf80a2681aef2d2859ca90911f
janith15745 <janeeth15745@gmail.com>
2025-03-22 15:05:22 +0530

    Add files via upload

---
+++ calc/calculator.js
@@ -0,0 +1,26 @@
+function Solve(a) {
+    var b = document.getElementById('res');
+    b.value += a;
+}
+function Result() {
+    var a = document.getElementById('res').value;
+    try {
+        var num = eval(a.replace('x', '*'));
+        document.getElementById('res').value = num;
+    }
+    catch (error) {
+        document.getElementById('res').value = "Error";
+    }
+}
+function Clear() {
+    document.getElementById('res').value = "";
+}
+
+function Back() {
+    var a = document.getElementById('res').value;
+    document.getElementById('res').value = a.slice(0, -1);
+}
+console.log(window.alert("you are using a basic calculator"));
+console.log(window.alert("it's janith"));
+
+
